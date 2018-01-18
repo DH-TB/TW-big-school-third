1cc2f5fdd9f09cabd8ea117ccc3d8af034503772
DH-TB <929659475@qq.com>
2018-01-18 22:35:16 +0800

    finish app test

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -1,15 +1,34 @@
+const stdin = require('mock-stdin').stdin();
 const App = require('../src/App');
 const AnswerGenerate = require('../src/AnswerGenerator');
 
 describe('app()',()=>{
+
+    beforeEach(()=> {
+        spyOn(AnswerGenerate, "randomGenerate").and.returnValue('1234');
+        spyOn(console, 'log');
+        spyOn(process, 'exit');
+        App.game();
+    });
+
     it('should return Congratulation when inputs is 1234 ',()=>{
 
-        spyOn(AnswerGenerate, "randomGenerate").and.returnValue('1234');
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6)');
 
-        spyOn(console,"log");
+        stdin.send('1234');
 
-        App.game();
-
-        expect(console.log).toEqual('Congratulation');
+        expect(console.log).toHaveBeenCalledWith('Congratulations!');
+        expect(process.exit).toHaveBeenCalled();
     })
+
+    it('should return illegal when input repeat number', ()=> {
+
+        expect(console.log).toHaveBeenCalledWith('Welcome!\n');
+        expect(console.log).toHaveBeenCalledWith('Please input your number(6)');
+
+        stdin.send('1134');
+
+        expect(console.log).toHaveBeenCalledWith('Cannot input illegal numbers!\n');
+    });
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-const scanf = require('scanf');
 const AnswerGenerate = require('./AnswerGenerator');
 const CompareNumber = require('./CompareNumber');
 
@@ -9,31 +8,34 @@ class App {
         let chance = 6;
         const answers = AnswerGenerate.randomGenerate();
 
-        while (chance > 0) {
-            App.print(chance);
+        this.print(chance);
 
-            const inputs = scanf('%s');
-            const array = inputs.split('');
-            const repeatNumberLength = array.filter((ele, i, arr) => arr.indexOf(ele) !== i).length;
-
-            if (array.length !== 4 || repeatNumberLength > 0) {
-                chance--;
-                App.illegal();
-            }
-            else if(inputs === answers){
-                App.success();
-                return;
-            }
-            else {
-                const result = CompareNumber.compareNumber(answers, inputs);
-                console.log(result + '\n');
-                chance--;
-                if (chance === 0) {
-                    App.failure();
-                    return;
+            process.stdin.resume();
+            process.stdin.setEncoding('utf8');
+            process.stdin.on('data', (inputs)=> {
+                const array = inputs.trim().split('');
+                const repeatNumberLength = array.filter((ele, i, arr) => arr.indexOf(ele) !== i).length;
+                
+                if (array.length !== 4 || repeatNumberLength > 0) {
+                    chance--;
+                    App.illegal();
+                    App.print(chance);
                 }
-            }
-        }
+                else if(inputs.trim() === answers){
+                    App.success();
+                    process.exit();
+                }
+                else {
+                    const result = CompareNumber.compareNumber(answers, inputs);
+                    console.log(result + '\n');
+                    chance--;
+                    if (chance === 0) {
+                        App.failure();
+                        process.exit();
+                    }
+                    App.print(chance);
+                }
+            })
     }
 
     static start() {
